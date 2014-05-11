055f951ca9ed5ba2ebdf6c675b2b72acef32e585
yingxi <yingxi@angelhere.com>
2014-05-11 15:18:57 +0800

    1

--- vmo/js/global.js
+++ vmo/js/global.js
@@ -42,29 +42,9 @@ var DURATION = 300;
 			$('.page').removeClass('current');
 			$(this).addClass('current');
 			$('#page1-ctrl1').xCenter();
-			// $('#page1-ctrl1').off('click');
-			// $('#page1-ctrl1').on('click', function() {
-			// var $this = $(this);
-			// $this.off('click');
-			// $this.animate({
-			// width : '30%',
-			// left : '35%',
-			// top : '50px'
-			// }, DURATION, function() {
-			// var $this = $(this);
-			// var t = $this.offset().top + $this.height() + 20;
-			// $('#page1-ctrl2').animate({
-			// top : t
-			// }, DURATION, function() {
-			// $(this).siblings('.downarr').fadeIn();
-			// });
-			// });
-			// });
-
 			$('#page1-ctrl1').swipe({
 				tap : function() {
 					var $this = $(this);
-					$this.off('click');
 					$this.animate({
 						width : '30%',
 						left : '35%',
@@ -83,27 +63,26 @@ var DURATION = 300;
 		});
 
 		$('#page2').bind('launch', function() {
-			$('.pg2sbt').off('click');
-			$('.pg2sbt').click(function() {
-				var $this = $(this).closest('.pg2sbt');
-				$this.siblings('.selected').removeClass('selected');
-				$this.addClass('selected');
-				$('#page2-ctrl3').attr('src', $this.find('img').attr('src'));
-
-				$('#page2-ctrl4').animate({
-					left : '-200px'
-				}, DURATION, function() {
-					var l1 = $this.attr('l1');
-					var l2 = $this.attr('l2');
-					$('#page2-ctrl4 .l1').html( l1 ? l1 : "");
-					$('#page2-ctrl4 .l2').html( l2 ? l2 : "");
+			$('.pg2sbt').swipe({
+				tap : function() {
+					var $this = $(this).closest('.pg2sbt');
+					$this.siblings('.selected').removeClass('selected');
+					$this.addClass('selected');
+					$('#page2-ctrl3').attr('src', $this.find('img').attr('src'));
 
 					$('#page2-ctrl4').animate({
-						left : '20px'
-					}, DURATION);
+						left : '-200px'
+					}, DURATION, function() {
+						var l1 = $this.attr('l1');
+						var l2 = $this.attr('l2');
+						$('#page2-ctrl4 .l1').html( l1 ? l1 : "");
+						$('#page2-ctrl4 .l2').html( l2 ? l2 : "");
 
-				});
-
+						$('#page2-ctrl4').animate({
+							left : '20px'
+						}, DURATION);
+					});
+				}
 			});
 			$(this).find('.downarr').fadeIn();
 		});
